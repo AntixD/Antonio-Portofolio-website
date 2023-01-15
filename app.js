ecf478cb38170ce2a9afc1d6fb95732babfce698
AntixD <antonio.dedic123@gmail.com>
2023-01-15 11:55:04 +0100

    about section
    start an about section with html css and also added js for the about section slider

---
+++ app.js
@@ -0,0 +1,14 @@
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab (tabName) {
+    for(tabLink of tabLinks) {
+        tabLink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents) {
+        tabContent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
+}
