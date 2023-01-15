691bbcba0701cc52a19e456d6eaa90dfabe2d0ed
AntixD <antonio.dedic123@gmail.com>
2023-01-15 17:57:26 +0100

    final touches
    everything has been completed on the website now need to add desired images and some final touches

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ function opentab (tabName) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
 }
+
+const sideMenu = document.getElementById("sidemenu");
+
+function openMenu () {
+    sideMenu.style.right = "0";
+}
+
+function closeMenu () {
+    sideMenu.style.right = "-200px";
+}
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzqGOkE3UWBeAg6zhZ-aDVMVyLw1L2RBXABOImtm3y16giqQcyv5-5MvZy5nvwadDel/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch (scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
